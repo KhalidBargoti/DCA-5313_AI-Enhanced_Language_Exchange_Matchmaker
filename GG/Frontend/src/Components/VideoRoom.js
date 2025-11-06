75bbb00ddc720b7a6704807686755ba1575730fb
nkirschner6 <NKIRSCHNER112@GMAIL.COM>
2025-11-06 15:54:23 -0500

    Token

--- GG/Frontend/src/Components/VideoRoom.js
+++ GG/Frontend/src/Components/VideoRoom.js
@@ -6,7 +6,7 @@ import { useNavigate, createSearchParams, useSearchParams } from 'react-router-d
 import './VideoRoom.css';
 
 const APP_ID = '50a71f096ba844e3be400dd9cf07e5d4';
-const TOKEN = 'YOUR_TOKEN_HERE'; // keep as configured
+const TOKEN = '007eJxTYOhhrnb9uYLpTaDLQwkDQ9vpG38Ufpk68XDbtiXpGz59ZAtQYDA1SDQ3TDOwNEtKtDAxSTVOSjUxMEhJsUxOMzBPNU0xeRcalN4QyMjw7tlCFkYGCATxuRlyE0uSM3ITszPz0hkYALNiJKE=';
 const CHANNEL = 'matchmaking';
 
 export const client = AgoraRTC.createClient({
