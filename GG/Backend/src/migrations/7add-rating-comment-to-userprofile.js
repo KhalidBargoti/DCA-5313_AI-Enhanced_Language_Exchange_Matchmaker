8b55aea0dc67dc74e99e8259e8bddf0cc96bb7f5
rebeccaasun <rebeccasun4@gmail.com>
2025-11-11 13:58:48 -0500

    fixed migration file minor capitalization error

--- GG/Backend/src/migrations/7add-rating-comment-to-userprofile.js
+++ GG/Backend/src/migrations/7add-rating-comment-to-userprofile.js
@@ -1,20 +1,20 @@
 export async function up(queryInterface, Sequelize) {
-  await queryInterface.addColumn('userProfile', 'rating', {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-    defaultValue: null,
-    comment: 'Rating given by other users after calls',
-  });
-
-  await queryInterface.addColumn('userProfile', 'comment', {
-    type: Sequelize.TEXT,
-    allowNull: true,
-    defaultValue: null,
-    comment: 'Comment given by other users after calls',
-  });
-}
-
-export async function down(queryInterface) {
-  await queryInterface.removeColumn('userProfile', 'rating');
-  await queryInterface.removeColumn('userProfile', 'comment');
-}
+    await queryInterface.addColumn('UserProfile', 'rating', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+      comment: 'Rating given by other users after calls',
+    });
+  
+    await queryInterface.addColumn('UserProfile', 'comment', {
+      type: Sequelize.TEXT,
+      allowNull: true,
+      defaultValue: null,
+      comment: 'Comment given by other users after calls',
+    });
+  }
+  
+  export async function down(queryInterface) {
+    await queryInterface.removeColumn('UserProfile', 'rating');
+    await queryInterface.removeColumn('UserProfile', 'comment');
+  }
